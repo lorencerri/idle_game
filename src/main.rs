2e1a8a6e502ee7764e20c84735f2248eb7293b7e
TrueXPixels <truexpixels@gmail.com>
2018-05-30 22:34:00 -0700

    Added Dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate serde_json;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+
+use std::error::Error;
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
+
+// Static Variables
+static DATA_FILE: &'static str = "data/data.json";
+
+#[derive(Serialize, Deserialize)]
+#[derive(Debug)]
+struct User {
+    name: String,
+    balance: i64,
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("{}", DATA_FILE);
+
 }
